de1f5ec7a5e09dbc9f41a66bb798f68f979f6d5c
= <39348867+ahmedibrahimhassan654@users.noreply.github.com>
2020-03-23 22:24:40 +0200

    after destructure

--- modules/company/controllers/index.js
+++ modules/company/controllers/index.js
@@ -1,1 +1,33 @@
-module.exports = {};
+const companiesController = require( './companies' );
+const emailConfirmationControler = require( './email-confirmation' ) 
+const emailVerificationControler = require( './email-verification' )
+
+const passwordForgetControler=require('./password-forget')  
+const loginController = require('./login');
+const successCallbackController = require('./success-callback');
+const verifyPhoneNumberController = require('./phone-verification');
+const confirmPhoneNumberController = require('./phone-confirmation');
+const verifyEmailController = require('./email-verification');
+const confirmEmailController = require('./email-confirmation');
+const forgetPasswordController = require('./password-forget');
+const resetPasswordController = require('./password-reset');
+
+module.exports = {
+  companiesController,
+  emailConfirmationControler,
+  emailVerificationControler,
+  verifyPhoneNumberController,
+  confirmPhoneNumberController,
+  verifyEmailController,
+  confirmEmailController,
+  forgetPasswordController,
+    resetPasswordController,
+  passwordForgetControler,
+  loginController,
+  successCallbackController
+  
+};
+
+
+
+
