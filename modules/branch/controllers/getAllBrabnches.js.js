d739d52a278da8d818cd4e877654224dc089c7e4
= <39348867+ahmedibrahimhassan654@users.noreply.github.com>
2020-03-29 21:23:01 +0200

    update branch

--- modules/branch/controllers/getAllBrabnches.js.js
+++ modules/branch/controllers/getAllBrabnches.js.js
@@ -6,16 +6,6 @@ const Branch = require('../branch.schema');
 // @route GET /api/v1/branch/:id
 // @route public
 exports.grtBranches = asyncHandler(async (req, res, next) => {
-//   let query;
-//   if (req.params.businessId) {
-//     query = Branch.find({ businessId: req.params.businessId });
-//   } else {
-//     query = Branch.find().populate({
-//       path: 'business',
-//       select: 'name'
-//     });
-//   }
-//   const branches = await query;
 
   res.status(200).json(res.advancedResults);
 });
--- modules/branch/controllers/index.js
+++ modules/branch/controllers/index.js
@@ -1,8 +1,10 @@
 const grtBranches = require('./getAllBrabnches.js');
 const getBranch = require('./getSingleBranch');
-const addBranch=require('./addBranch')
+const addBranch = require( './addBranch' )
+const updateBranch=require('./updateBranch')
 module.exports = {
     grtBranches,
     getBranch, 
-    addBranch
+    addBranch,
+    updateBranch
 };
