478dcd872cff5e79f215bdf1dc4b1ebcebb56b4f
= <39348867+ahmedibrahimhassan654@users.noreply.github.com>
2020-03-29 20:56:52 +0200

    add branch controer

--- modules/branch/controllers/addBranch.js
+++ modules/branch/controllers/addBranch.js
@@ -8,52 +8,23 @@ const { ROLE_BRANCH_MANAGER } = require('../../user/enum/roles');
 //@desc  add branch
 //@route POST /api/v1/businesses/:businessId/branches
 //@route private
-// exports.addBranch = asyncHandler(async (req, res, next) => {
-//     req.body.businessId = req.params.businessId
-
-//     const business = await Business.findById( req.params.businessId )
-
-//     if ( !business )
-//     {
-//         return next(
-//           new ErrorResponse(`no branch with the id of ${req.params.businessId}`),
-//           404
-//         );
-//     }
-//     const branch=await Branch.create(req.body)
-//       res.status(200).json({
-//         sucess: true,
-//         data: branch
-//       });
-
-//     })
 
 exports.addBranch = asyncHandler(async (req, res, next) => {
-  let { name, address, managerId } = req.body;
-
-  // Create Business admin
-  const newMang = new User({
-    ...managerId,
-    roles: [ROLE_BRANCH_MANAGER]
-  });
-  const newName = new Branch({
-    ...name
-  });
-  const newaddress = new Branch({
-    ...address
-  });
   req.body.businessId = req.params.businessId;
 
   const business = await Business.findById(req.params.businessId);
+  const branchManger = await Branch.find(req.body.managerId);
 
   if (!business) {
     return next(
-      new ErrorResponse(`no branch with the id of ${req.params.businessId}`),
+      new ErrorResponse(
+        `no businessId with the id of ${req.params.businessId}`
+      ),
       404
     );
   }
 
-  const branch = await Branch.create(req.body);
+  const branch = await Branch.create(req.body, branchManger);
 
   res.status(200).json({
     sucess: true,
