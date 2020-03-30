534bf343dd544a1270986598634dbdd87e5d087f
= <39348867+ahmedibrahimhassan654@users.noreply.github.com>
2020-03-30 12:28:49 +0200

    update Branch

--- modules/branch/controllers/updateBranch.js
+++ modules/branch/controllers/updateBranch.js
@@ -1,13 +1,46 @@
+// const ErrorResponse = require('../../../common/utils/errorResponse');
+// const asyncHandler = require('../../../common/middleware/async');
+// const Branch = require('../branch.schema');
+
+
+// //@desc  upadte branch
+// //@route put /api/v1/api/v1/branches/:id
+// //@route private
+
+// exports.updateBranch = asyncHandler(async (req, res, next) => {
+//     let branch = await Branch.findById(req.params.id);
+//     if (!branch) {
+//       return next(
+//         new ErrorResponse(`no branch with the id of ${req.params.id}`),
+//         404
+//       );
+//     }
+//     branch = await Branch.findByIdAndUpdate( req.params.id, req.body, {
+//         new: true,
+//         runValidators:true
+//     })
+//   res.status( 200 ).json( {
+//       message:'branch updated ',
+//       sucess: true,
+//       data: branch
+//     });
+//   });
+  
+const {
+  CREATED,
+  BAD_REQUEST,
+  INTERNAL_SERVER_ERROR
+} = require('http-status-codes');
+
 const ErrorResponse = require('../../../common/utils/errorResponse');
 const asyncHandler = require('../../../common/middleware/async');
 const Branch = require('../branch.schema');
 
+module.exports = asyncHandler( async ( req, res, next ) =>
+{
+  
 
-//@desc  upadte branch
-//@route put /api/v1/api/v1/branches/:id
-//@route private
-
-exports.updateBranch = asyncHandler(async (req, res, next) => {
+  try {
     let branch = await Branch.findById(req.params.id);
     if (!branch) {
       return next(
@@ -19,10 +52,20 @@ exports.updateBranch = asyncHandler(async (req, res, next) => {
         new: true,
         runValidators:true
     })
-  res.status( 200 ).json( {
-      message:'branch updated ',
-      sucess: true,
-      data: branch
+
+    return res.status(CREATED).json({
+      status: true,
+      message: 'branch updated successfully',
+      data: null
     });
-  });
+  } catch (err) {
   
+    return next(
+      new ErrorResponse(
+        err.message,
+        err.status || INTERNAL_SERVER_ERROR,
+        err.stack
+      )
+    );
+  }
+});
