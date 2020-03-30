705b8811c9642ed7d1091262b503e2894b640bcf
Mohammad Sakr <mohammad_sakr_89@yahoo.com>
2020-03-30 02:06:22 +0200

    Refactor add branch

--- modules/business/business.routes.js
+++ modules/business/business.routes.js
@@ -3,18 +3,10 @@ const express = require('express');
 const requestValidator = require('../../common/middleware/requestValidator');
 const isAuthorized = require('../../common/middleware/isAuthorized');
 const { healthyCheck } = require('./controllers');
-//include other resource routes
-const branchRouter = require( '../branch/branches.routes' );
-
 
 const {} = require('./joi/validationSchemas');
 
-const {} = require('./endPoints');
-
 const router = express.Router();
-
-//reroute into other resource router
-router.use('/:businessId/branches', branchRouter);
 
 router.get('/', healthyCheck);
 
