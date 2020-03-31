9b0b007355d1c158fbb3e93e5ef200344e507c70
= <39348867+ahmedibrahimhassan654@users.noreply.github.com>
2020-03-31 23:35:25 +0200

    add product

--- modules/product/controllers/index.js
+++ modules/product/controllers/index.js
@@ -1,5 +1,7 @@
 const healthyCheck = require('./healthy');
+const addProductController = require('./addProduct');
 
 module.exports = {
-  healthyCheck
+  healthyCheck,
+  addProductController
 };
---
+++ modules/product/enum/currency.js
@@ -0,0 +1,7 @@
+const currency = Object.freeze({
+  USD: 0,
+  EUR: 1,
+  EGP: 2,
+  });
+  
+  module.exports = currency;
